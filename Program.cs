194ed5159b79cf426af1635a86bd4170cefad176
Yegana Hasanova <yeganamha@code.edu.az>
2023-06-16 19:44:45 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTrim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   string str = Console.ReadLine();
+            
+            TrimMethods.Trim(str);
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ TrimMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTrim
+{
+    public static class TrimMethods
+    {
+        public static void Trim(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    string[] TrimedArr = new string[] { };
+
+                    for (int j = 0; j < TrimedArr.Length; j++)
+                    {
+                        Console.WriteLine(TrimedArr);
+                    }
+                }
+               
+                
+            }
+            
+            
+        }
+
+    }
+}
